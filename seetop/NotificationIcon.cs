e6ee543c8566137edb016278ba99e8722d819f3d
Student Ivan <cryptox3@gmail.com>
2012-04-23 20:29:30 +0400

    first commit

---
+++ seetop/NotificationIcon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace seetop
+{
+	public sealed class NotificationIcon
+	{
+		private NotifyIcon notifyIcon;
+		private ContextMenu notificationMenu;
+		private static System.Threading.Timer ticker;
+		
+		public NotificationIcon()
+		{
+			notifyIcon = new NotifyIcon();
+			notificationMenu = new ContextMenu(InitializeMenu());
+			
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationIcon));
+			notifyIcon.Icon = (Icon)resources.GetObject("$this.Icon");
+			notifyIcon.ContextMenu = notificationMenu;
+			ticker = new System.Threading.Timer(TimerMethod, null, 5000, 15 * 60 * 1000);
+		}
+		
+		private MenuItem[] InitializeMenu()
+		{
+			MenuItem[] menu = new MenuItem[] {
+				new MenuItem("Exit", menuExitClick)
+			};
+			return menu;
+		}
+
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			
+			bool isFirstInstance;
+			using (Mutex mtx = new Mutex(true, "seetop", out isFirstInstance)) {
+				if (isFirstInstance) {
+					NotificationIcon notificationIcon = new NotificationIcon();
+					notificationIcon.notifyIcon.Visible = true;
+					Application.Run();
+					notificationIcon.notifyIcon.Dispose();
+				} else {
+					MessageBox.Show("Already running");
+				}
+			}
+		}
+		
+		public static void TimerMethod(object state)
+		{
+			Console.Beep(400, 1500);
+		}
+		
+		private void menuExitClick(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}
